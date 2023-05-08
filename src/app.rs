3747e04ae54135781af57446b655ea95611097ad
Viktor Dudnik <jackfilov@gmail.com>
2023-05-08 11:13:51 +0300

    feat: pagination and display limit

--- src/app.rs
+++ src/app.rs
@@ -40,15 +40,21 @@ impl Default for OpenFileOptions {
   }
 }
 
+const PAGE_STEP: usize = 50usize;
+
 #[function_component(App)]
 pub fn app() -> Html {
   let open_trigger = use_state(|| false);
   let table_headers = use_state(|| vec![]);
   let table_data = use_state(|| vec![vec![]]);
+  let skip_amount = use_state(|| 0usize);
+  let load_limit = use_state(|| PAGE_STEP);
   {
     let table_headers = table_headers.clone();
     let table_data = table_data.clone();
     let open_trigger = open_trigger.clone();
+    let load_limit = load_limit.clone();
+    let skip_amount = skip_amount.clone();
     use_effect(move || {
       spawn_local(async move {
         if !*open_trigger {
@@ -58,6 +64,8 @@ pub fn app() -> Html {
         let options = to_value(&OpenFileOptions::default()).unwrap();
         let file = open(options).await;
         open_trigger.set(false);
+        skip_amount.set(0);
+        load_limit.set(PAGE_STEP);
         match file.as_string() {
           Some(path) => {
             let data = readTextFile(&path).await.as_string().unwrap();
@@ -86,6 +94,40 @@ pub fn app() -> Html {
     })
   };
 
+  let load_more = {
+    let load_limit = load_limit.clone();
+    Callback::from(move |e: MouseEvent| {
+      e.prevent_default();
+      load_limit.set(*load_limit + PAGE_STEP);
+    })
+  };
+
+  let load_all = {
+    let load_limit = load_limit.clone();
+    let skip_amount = skip_amount.clone();
+    Callback::from(move |e: MouseEvent| {
+      e.prevent_default();
+      skip_amount.set(0);
+      load_limit.set(usize::MAX);
+    })
+  };
+
+  let next_page = {
+    let skip_amount = skip_amount.clone();
+    Callback::from(move |e: MouseEvent| {
+      e.prevent_default();
+      skip_amount.set(*skip_amount + PAGE_STEP);
+    })
+  };
+
+  let prev_page = {
+    let skip_amount = skip_amount.clone();
+    Callback::from(move |e: MouseEvent| {
+      e.prevent_default();
+      skip_amount.set(*skip_amount - PAGE_STEP);
+    })
+  };
+
   html! {
     <main class="container">
       <button onclick={open_file} type="submit">{"Open"}</button>
@@ -95,7 +137,7 @@ pub fn app() -> Html {
             <th>{ header }</th>
           }).collect::<Html>()
         }
-        { table_data.iter().map(|row| 
+        { table_data.iter().skip(*skip_amount).take(*load_limit).map(|row| 
           html! {
             <tr>
               { row.iter().map(|cell| {
@@ -107,6 +149,32 @@ pub fn app() -> Html {
           }).collect::<Html>()
         }
       </table>
+      {
+        if !table_headers.is_empty() {
+          html!{
+            <div class="navigation">
+              if table_data.len() > *load_limit {
+                <span>{"Page: "}{*skip_amount / PAGE_STEP + 1}{"/"}{table_data.len() / *load_limit}</span>
+                if *skip_amount != 0 {
+                  <button onclick={prev_page} type="submit">{"Prev page"}</button>
+                }
+                <button onclick={next_page} type="submit">{"Next page"}</button>
+                <button onclick={load_more} type="submit">{"Load more"}</button>
+                <button onclick={load_all} type="submit">{"Load all"}</button>
+              }
+              <span>{"Rows: "}
+                if table_data.len() < *load_limit {
+                  {table_data.len()}
+                } else {
+                  {*load_limit}{"/"}{table_data.len()}
+                }
+              </span>
+            </div>
+          }
+        } else {
+          html!{}
+        }
+      }
     </main>
   }
 }
